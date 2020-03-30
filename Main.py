5965aef55620a4acdbe27a0d4d893b5784d3ed91
SandelsCol <62896050+SandelsCol@users.noreply.github.com>
2020-03-30 16:47:46 -0500

    Solucion

--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ b=int(input("Digite B numero"))
 c=int(input("Digite C numero"))
 d=int(input("Digite D numero"))
 
+lol=b**2-(4*a*c)
+
 Result= a*c
 Doble= 2*a
 Cuadrado = b**2
@@ -12,4 +14,19 @@ razo=math.sqrt(d)
 print("El producto entre los numeros a y c es..",Result)
 print("El doble del numero a es...",Doble)
 print("El cuadrado del numero b es..",Cuadrado)
-print("La razi del numero d es..",razo)
+print("La raiz del numero d es..",razo)
+
+if lol>0:
+    x1=(-b+math.sqrt(lol))/2*a
+    x2=(-b-math.sqrt(lol))/2*a
+    print("Los dos valores son...",x1,x2) 
+elif lol==0:
+    x3=-b/2*a
+    print("El valor de x es de..",x3)
+else:
+    print("No existe solución a la ecuación cuadrática dentro del dominio de los números reales")
+
+
+
+
+
