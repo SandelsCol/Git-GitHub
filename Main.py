586ce0fa793b09a58babc5f40febb1dbd23d4eed
SandelsCol <62896050+SandelsCol@users.noreply.github.com>
2020-03-30 15:05:41 -0500

    mi primera modificacion

--- Main.py
+++ Main.py
@@ -1,8 +1,15 @@
+import math
 a=int(input("Digite A numero"))
+b=int(input("Digite B numero"))
 c=int(input("Digite C numero"))
+d=int(input("Digite D numero"))
 
 Result= a*c
 Doble= 2*a
+Cuadrado = b**2
+razo=math.sqrt(d)
 
 print("El producto entre los numeros a y c es..",Result)
 print("El doble del numero a es...",Doble)
+print("El cuadrado del numero b es..",Cuadrado)
+print("La razi del numero d es..",razo)
