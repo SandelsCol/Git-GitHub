31fa126fcbb03c499910fb6cdddc6f8c567fa796
SandelsCol <62896050+SandelsCol@users.noreply.github.com>
2020-03-30 14:48:48 -0500

    Mi primer commit

---
+++ Main.py
@@ -0,0 +1,8 @@
+a=int(input("Digite A numero"))
+c=int(input("Digite C numero"))
+
+Result= a*c
+Doble= 2*a
+
+print("El producto entre los numeros a y c es..",Result)
+print("El doble del numero a es...",Doble)
